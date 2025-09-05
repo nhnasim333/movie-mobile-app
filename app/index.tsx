d827cd3496d6f937d22554fd1ae70a57a81365b4
Nasim <mdnasimhosen333@gmail.com>
2025-09-05 20:10:49 +0600

    Update Tailwind CSS version and adjust configuration paths

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome to my Movie App!</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-bold text-blue-600">
+        Welcome to my Movie App!
+      </Text>
     </View>
   );
 }
