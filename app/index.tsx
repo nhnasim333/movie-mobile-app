d3b4745a643e6d10ed334011657938cc24cdb7d2
Nasim <mdnasimhosen333@gmail.com>
2025-09-05 20:24:25 +0600

    Update text color in Index component and extend Tailwind CSS theme colors

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-bold text-blue-600">
+      <Text className="text-3xl font-bold text-dark-200">
         Welcome to my Movie App!
       </Text>
     </View>
