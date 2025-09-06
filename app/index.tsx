dd870b90b0644b6003a0697d3c272835e8a66aa4
Nasim <mdnasimhosen333@gmail.com>
2025-09-06 09:05:14 +0600

    Update welcome message in Index component and add Onboarding screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-3xl font-bold text-dark-200">
-        Welcome to my Movie App!
+        Welcome to Movie App!
       </Text>
+      <Link href="/onboarding">
+        <Text>Go to Onboarding</Text>
+      </Link>
     </View>
   );
 }
